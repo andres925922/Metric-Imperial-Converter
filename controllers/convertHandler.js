1cf7e16b6bde824b633f36a3b35bed89595db74b
Andres Convertini <andres.convertini91@gmail.com>
2023-01-03 20:30:35 -0300

    second approach

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,28 +1,88 @@
 // serializar el input en función de un regex
 
 const dataSplitter = (data) => {
+  /**
+   * Función que permite, en función de la data pasada, devolver el número y la unidad enviada por el cliente
+   * data @type {string}
+   * @type {Array}
+   */
   const number = input.match(/[.\d\/]+/g) || ["1"];
   const string = input.match(/[a-z][A-Z]+/g)[0];
 
   return [number[0], string]
 }
 
+const checkDivision = (potentialFraction) => {
+  /**
+   * Función que permite checkear división de 2 números si se ingresa 
+   * 2/3 -> return ["2","3"]
+   * 2 -> ["2"]
+   * 2/3/3 -> false
+   */
+  const nums = potentialFraction.split('/'); // En caso de que exista un dígito /
+  if(nums.length > 2){ // Si 
+    return false
+  }
+  return nums
+}
+
+const defUnits = {
+  km: ["mi", "kilometers"],
+  mi: ["km", "miles"],
+
+  gal: ["L", "galons" ],
+  L: ["gal", "liters"],
+
+  lbs: ["kg", "pounds"],
+  kg: ["lbs", "kilograms"],
+
+}
+
+const units = Object.keys(defUnits);
+
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
+    const data = dataSplitter(input)[0];
+    const nums = checkDivision(data);
+
+    if(!nums){
+      return undefined
+    }
+    
+    const num1 = nums[0];
+    const num2 = nums[1] || "1";
+
+    if(isNaN(num1) || isNaN(num2)){
+      return undefined
+    }
+
+    const result = parseFloat(num1) / parseFloat(num2);
     
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
+    const result = dataSplitter(input)[1].toLowerCase(); // We should pass it as Lower Case
     
+    // Check if the result is in the array of units
+    if (!units.includes(result)){
+      return undefined
+    }
+
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
+    const _unit = initUnit.toLowerCase();
+    const unitData = units.find(unit => {
+      // if (defUnits[unit][0] === "l"){
+      //   return "L"
+      // }
+      unit === _unit
+    })[0];
+
+
     
     return result;
   };
